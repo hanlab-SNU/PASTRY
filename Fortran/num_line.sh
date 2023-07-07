8dc5aa48492c96f21c4bd1775564ac66e1cd059b
Chloe Soohyun Jang <74101107+chloe-shjang@users.noreply.github.com>
2023-07-07 09:54:22 +0900

    Delete Fortran directory

--- Fortran/num_line.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash                                                                                             
-
-awk '{
-      numline = numline + 1
-     } END{
-           printf("%8.1f\n", numline);
-          }' "$1"
-
